64a454cf46afe7c07c40ec69923e671263877ced
A. Henrie <103060170+ahenrie@users.noreply.github.com>
2023-06-20 17:02:32 -0600

    Update dockerconfig.py

--- dockerconfig.py
+++ dockerconfig.py
@@ -1,3 +1,5 @@
+#SAMPLE FILE -> Usefull if we wanted to add function for docker to main.
+
 import subprocess
 
 def create_postgres_container():
@@ -7,3 +9,4 @@ def create_postgres_container():
 def destroy_container():
 # Stop and remove the PostgreSQL container using Docker Compose
     subprocess.run(['docker-compose', 'down'])
+
